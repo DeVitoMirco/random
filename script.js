45cc87d41d202339b98ca60758d0f649490f6d2b
DeVitoMirco <38210805+DeVitoMirco@users.noreply.github.com>
2020-04-08 21:14:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -28,12 +28,12 @@ $(document).ready(function () {
             var x = event.pageX;
             var y = event.pageY;
             var nextItem = i + 1;
-            var prevItem = nextItem - 2;
+            var prevItem = nextItem - 3;
             if (nextItem >= $('.page').length) {
                 nextItem = 0;
             }
-            if (nextItem == 1 || nextItem == 2)  {
-                prevItem = nextItem + $('.page').length - 2;
+            if (prevItem == 0 || prevItem == -1 || prevItem == -2)  {
+                prevItem = prevItem + $('.page').length;
             } 
 
             $('.page:eq(' + nextItem + ')').css('z-index', parseInt($(this).css('z-index')) + 1);
